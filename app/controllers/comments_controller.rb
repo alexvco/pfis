8099e2003e8ad109909d4a683e4c620c81480a68
alexv7 <alexvarjabedian7@gmail.com>
2016-08-19 01:11:35 -0400

    added controller action create for comment to be created for a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+before_action :set_post
+
+def create
+  @post.comments.create!(comment_params)
+  redirect_to @post
+end
+
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
