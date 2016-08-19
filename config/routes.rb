017b392d66968ed683708eec1aae429a04991050
alexv7 <alexvarjabedian7@gmail.com>
2016-08-18 22:13:34 -0400

    changed or rather added /login in routes, which is the same as /users/sign_in (new_user_session)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'posts#index'
 
+  as :user do
+    get "/login" => "devise/sessions#new"
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
